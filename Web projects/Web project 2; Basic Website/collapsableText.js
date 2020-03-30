d3f3467e008223430dfc953c94a5f3a6b673be2b
Joshua Lebert <expertjosh2000@gmail.com>
2020-03-30 16:31:52 -0400

    final touch ups

--- Web projects/Web project 2; Basic Website/collapsableText.js
+++ Web projects/Web project 2; Basic Website/collapsableText.js
@@ -1,15 +1,19 @@
-function collapse(obj, content) {
+function collapse(obj, content, id, coll) {
     obj.classList.toggle("active");
     if (document.getElementById(content).style.display === "block") {
         document.getElementById(content).style.display = "none";
     } else {
         document.getElementById(content).style.display = "block";
     }
+    
+    var navBar = document.getElementById(id);
+    var collapseContainer = document.getElementsByClassName(coll);
+    navBar.style.height = concat(collapseContainer.offsetHeight, "px");
+
 }
 
 function alignCollapse() {
-    var navBar = document.getElementById("navBar");
-    var collapseContainer = document.getElementsByClassName("collapseContainer");
-    navBar.style.height = collapseContainer.offsetHeight;
+
+   
 }
 window.onload = alignCollapse();
